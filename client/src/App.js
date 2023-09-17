a9ed5f186ae71dc0a513cc96f846f450340576e2
SahejChandok <chandoksahej@gmail.com>
2023-09-17 17:27:07 -0400

    Homepage using react and insatllation.

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href=" " className="logo">My Blog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className='image'>
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" alt=" "/>
+        </div>
+        <div className="texts">
+          <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+          <p className='info'>
+            <a className="author">Sahej Singh</a>
+            <time>2023-09-17 17:20</time>
+          </p>
+          <p className='summary'>Today at it's special event we'll be talking about the tech start ups that will help us build battery back up start ups</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className='image'>
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" alt=" "/>
+        </div>
+        <div className="texts">
+          <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+          <p className='info'>
+            <a className="author">Sahej Singh</a>
+            <time>2023-09-17 17:20</time>
+          </p>
+          <p className='summary'>Today at it's special event we'll be talking about the tech start ups that will help us build battery back up start ups</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className='image'>
+          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" alt=" "/>
+        </div>
+        <div className="texts">
+          <h2>EcoFlow teases full-house battery backup coming later this year</h2>
+          <p className='info'>
+            <a className="author">Sahej Singh</a>
+            <time>2023-09-17 17:20</time>
+          </p>
+          <p className='summary'>Today at it's special event we'll be talking about the tech start ups that will help us build battery back up start ups</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
